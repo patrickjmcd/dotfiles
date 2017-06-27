96d214b33a7133ccc828f7a87b6b163402961e91
Patrick McDonagh <patrickjmcd@gmail.com>
2017-06-27 12:25:42 -0500

    Install node, python, ruby, and rails

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 echo "Setting up everything for Linux"
-# in case we are in bash
-
 
 echo "Lets first make sure our package list is up to date"
 sudo apt-get update
 
-echo "Installing zsh"
-sudo apt-get install -y zsh
+echo "Remove some stupid packages that aren't needed"
+sudo apt-get -y remove --purge minecraft-pi python-minecraftpi wolfram-engine sonic-pi scratch nuscratch libreoffice
+sudo apt-get -y autoremove
 
-echo "Switching to zsh..."
-zsh
+echo "Upgrade any packages that are out of date"
+sudo apt-get -y upgrade
+
+echo "Installing zsh & tmux"
+sudo apt-get install -y zsh tmux
 
 # go home
 echo "Going home..."
@@ -60,12 +62,31 @@ if [ -f ~/.gitconfig ]; then
     echo "Overriding .gitconfig..."
     rm ~/.gitconfig
 fi
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 
 if [ -f ~/.zshrc ]; then
     echo "Overriding .zshrc..."
     rm ~/.zshrc
 fi
 ln -s ~/dotfiles/zshrc ~/.zshrc
+
+echo "Installing nvm and node"
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install node
+
+echo "Installing python versions"
+sudo apt-get install -y python python-pip python3 python3-pip
+
+echo "Installing RVM and Ruby"
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+source /home/patrickjmcd/.rvm/scripts/rvm
+rvm install ruby --latest
+gem install rails
+
 
 # in case we are in bash...
 echo "Changing shell to zsh..."
--- zshrc
+++ zshrc
@@ -1,12 +1,12 @@
 # Path to your oh-my-zsh installation.
-export ZSH=/Users/patrickjmcd/.oh-my-zsh
+export ZSH=~/.oh-my-zsh
 
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
 ZSH_THEME="agnoster"
 
-plugins=(git brew python docker pip tmux aws osx)
+plugins=(git python docker pip tmux aws npm node)
 
 source $ZSH/oh-my-zsh.sh
 
