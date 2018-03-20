28501676c4c9ad8ebf5c985350e20af93526f6b8
Patrick McDonagh <patrickjmcd@gmail.com>
2018-03-20 18:47:42 -0500

    install rbenv vs RVM

--- setup.sh
+++ setup.sh
@@ -114,10 +114,9 @@ brew_setup(){
   #  command line tool for Heroku
   brew_check heroku
 
-  # RVM
-  #  keep your rubies from stepping on one another
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-  \curl -sSL https://get.rvm.io | bash -s stable
+  # rbenv
+  #  Ruby Environment
+  brew_check rbenv
 
   # emacs
   #  text editor
@@ -197,10 +196,9 @@ aptget_setup(){
   #  command line tool for Heroku
   sudo apt-get install heroku
 
-  # RVM
+  # rbenv
   #  keep your rubies from stepping on one another
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-  \curl -sSL https://get.rvm.io | bash -s stable
+  sudo apt-get install rbenv
 
   # emacs
   #  text editor
